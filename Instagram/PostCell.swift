7b985657f4671dc14e96a0e28345b56d70cbfa95
panncherry <maung.eainmatmay@gmail.com>
2019-03-21 08:57:06 -0400

    added App Transport Security Settings

---
+++ Instagram/PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  Instagram
+//
+//  Created by Pann Cherry on 3/20/19.
+//  Copyright © 2019 TechBloomer. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
